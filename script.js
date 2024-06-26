ed0eea511e64a99cd2423c083935638bd3b3ed91
krig6 <ej.ldsm@gmail.com>
2024-06-26 23:23:29 +0800

    feat: Implement modal visibility toggle on addBookButton click
    Added JavaScript to toggle the visibility of the modal-overlay and adjust body overflow when addBookButton is clicked.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const modalContainer = document.querySelector('[data-modal-container]');
 const addBookButton = document.querySelector('[data-add-book]');
+const modalOverlay = document.querySelector('[data-overlay]');
 
 addBookButton.addEventListener('click', () => {
     modalContainer.classList.toggle('show-modal');
+
+    if (modalContainer.classList.contains('show-modal')) {
+        modalOverlay.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    } else {
+        modalOverlay.style.display = 'none';
+        document.body.style.overflow = 'hidden';
+    }
 })
