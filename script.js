437d67a77005b305abf77e5e10194a3e74c0c7a7
krig6 <ej.ldsm@gmail.com>
2024-06-27 18:30:30 +0800

    chore: Remove leading spaces before author names in predefined book data

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ const bookListContainer = document.querySelector('[data-book-list]');
 class Library {
     constructor() {
         this.book = [
-            { title: "The Great Gatsby", author: '	F. Scott Fitzgerald', pages: 192, status: false },
-            { title: "The Lord of the Rings", author: '	J. R. R. Tolkien', pages: 1077, status: true },
+            { title: "The Great Gatsby", author: 'F. Scott Fitzgerald', pages: 192, status: false },
+            { title: "The Lord of the Rings", author: 'J. R. R. Tolkien', pages: 1077, status: true },
             { title: "Harry Potter and the Prisoner of Azkaban", author: 'J. K. Rowling', pages: 317, status: true },
             { title: "The Book Thief", author: 'Markus Zusak', pages: 584, status: false },
             { title: "Moby-Dick", author: 'Herman Melville', pages: 635, status: false },
