7bfa979965f3e7f59f1076b0da6224504828c811
krig6 <ej.ldsm@gmail.com>
2024-06-29 18:55:25 +0800

    refactor: Initialize Library constructor with an empty array
    Removed initial book data initialization in favor of displaying an empty library message when no books are present.

--- script.js
+++ script.js
@@ -35,13 +35,7 @@ function truncateString(text, maxLength) {
 
 class Library {
     constructor() {
-        this.book = this.retrieveBooks() || [
-            { title: "The Great Gatsby", author: 'F. Scott Fitzgerald', pages: 192, status: false },
-            { title: "The Lord of the Rings", author: 'J. R. R. Tolkien', pages: 1077, status: true },
-            { title: "Harry Potter and the Prisoner of Azkaban", author: 'J. K. Rowling', pages: 317, status: true },
-            { title: "The Book Thief", author: 'Markus Zusak', pages: 584, status: false },
-            { title: "Moby-Dick", author: 'Herman Melville', pages: 635, status: false },
-        ]
+        this.book = this.retrieveBooks() || [];
         this.refreshBookDisplay();
     }
 
